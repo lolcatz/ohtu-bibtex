efd1122fa72000ecf2d9c13cf30c5fa16a6dc19b
Noemj <Noemj@Noemj-PC.(none)>
2012-04-19 21:28:06 +0300

    Added the service test

--- src/main/java/Bibtex/domain/Reference.java
+++ src/main/java/Bibtex/domain/Reference.java
@@ -29,6 +29,10 @@ public class Reference implements Serializable {
     public String getType() { return type; }
     public Long getId() { return id; }
     public HashMap<String,String> getFields() { return fields; }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
     
     public void setKey(String key) {  
         this.key = key; 
